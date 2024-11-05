8d2028f8758b597fec09437f8089eda2fc54c516
Aryan <coolaryanbajpai20@gmail.com>
2024-11-05 11:52:34 +0530

    discard elements

--- app/sorting/BubbleSort/page.tsx
+++ app/sorting/BubbleSort/page.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import BubbleSortPage from "./BubbleSortPage";
-export default function pages() {
+"use client";
+import { useState } from "react";
+import ArrayControls from "@/components/controls/ArrayControls";
+import BubbleSortVisualizer from "@/components/visualizer/sorting/BubbleSort";
+
+export default function BubbleSortPage() {
+  const [array, setArray] = useState<number[]>([5, 3, 8, 4, 2]);
+
   return (
-    <div>
-      <BubbleSortPage />
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl text-center font-bold text-white mb-8">
+        Bubble Sort Visualization
+      </h1>
+      <ArrayControls onArrayChange={setArray} />
+      <BubbleSortVisualizer array={array} />
     </div>
   );
 }
